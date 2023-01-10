431aefe711dc660e144edc7b3ddd7fd69ea3eb8b
ebenyoub <ebenyoub@me.com>
2023-01-11 00:38:40 +0100

    Change sizes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import AddBook from "./components/AddBook";
 import SearchBook from "./components/SearchBook";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -7,12 +8,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
-                    <Header />
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <Header />
+                <main className="main">
                     <Routes>
                         <Route path="/" element={<AddBook />} />
                         <Route path="search" element={<SearchBook />} />
                     </Routes>
+                </main>
+                <Footer />
             </BrowserRouter>
         </div>
     );
